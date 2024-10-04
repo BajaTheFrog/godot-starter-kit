0a366a41d309d5f26cc73044f5c61c707b1b06bf
Baja The Frog <bajathefrog@gmail.com>
2024-10-04 17:12:07 -0400

    Remove references to constants

--- game/services/world_service.gd
+++ game/services/world_service.gd
@@ -4,7 +4,7 @@ class_name WorldService
 #var top_most_world_node: Node2D = null
 
 #func destroy_all_enemies() -> void:
-#	var enemies = get_tree().get_nodes_in_group(Game.constants.roots.enemy)
+#	var enemies = get_tree().get_nodes_in_group(Game.groups.roots.enemy)
 #	for enemy in enemies:
 #		var true_enemy = enemy as EnemyRoot
 #		if true_enemy:
@@ -12,7 +12,7 @@ class_name WorldService
 #
 #
 #func destroy_all_bullets() -> void:
-#	var bullets = get_tree().get_nodes_in_group(Game.constants.roots.bullet)
+#	var bullets = get_tree().get_nodes_in_group(Game.groups.roots.bullet)
 #	for bullet in bullets:
 #		var true_bullet = bullet as Bullet
 #		if true_bullet:
