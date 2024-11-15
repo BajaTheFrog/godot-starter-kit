0622036caebf17237c2043847c0ab2bbd2df3794
Baja The Frog <bajathefrog@gmail.com>
2024-11-15 18:03:00 -0500

    Update state and state machine
    From StateCharacterDemo

--- addons/first_party/states/state.gd
+++ addons/first_party/states/state.gd
@@ -1,36 +1,46 @@
 # Virtual base class for all states.
-class_name State
 extends Node
+class_name State
 
 # Reference to the state machine, to call its `transition_to()` method directly.
 # That's one unorthodox detail of our state implementation, as it adds a dependency between the
 # state and the state machine objects, but we found it to be most efficient for our needs.
 # The state machine node will set it.
-var state_machine = null
-var is_in_state = false
+var state_machine: StateMachine = null
+var is_in_state: bool = false
+var id: String = ""
 
 # Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
 # is a dictionary with arbitrary data the state can use to initialize itself.
-func enter(_properties := {}) -> void:
+func enter(previous_state_id: String, properties := {}) -> void:
 	is_in_state = true
-
-
-# Virtual function. Receives events from the `_unhandled_input()` callback.
-func handle_input(_event: InputEvent) -> void:
-	pass
-
+	
 
 # Virtual function. Corresponds to the `_process()` callback.
-func update(_delta: float) -> void:
+func process_update(delta: float) -> void:
 	pass
 
 
 # Virtual function. Corresponds to the `_physics_process()` callback.
-func physics_update(_delta: float) -> void:
+func physics_update(delta: float) -> void:
+	pass
+	
+
+# Virtual function. Receives events from the `_unhandled_input()` callback.
+func input_update(event: InputEvent) -> void:
+	pass
+
+
+# Virtual function. Receives events from the `_unhandled_input()` callback.
+func unhandled_input_update(event: InputEvent) -> void:
 	pass
 
 
 # Virtual function. Called by the state machine before changing the active state. Use this function
 # to clean up the state.
-func exit() -> void:
+func exit(next_state_id: String) -> void:
 	is_in_state = false
+	
+	
+func go_to(new_state_id: String, properties: Dictionary = {}) -> void:
+	state_machine.transition_to(new_state_id, properties)
